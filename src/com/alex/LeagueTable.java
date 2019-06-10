90559d266a6b6b69b8f0ee60691f85432e1cefd6
Leon <stbirb@yandex.ru>
2019-06-10 17:34:05 +0300

    add code

--- src/com/alex/LeagueTable.java
+++ src/com/alex/LeagueTable.java
@@ -1,4 +1,27 @@
 package com.alex;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class LeagueTable<T extends Team> {
+    ArrayList<T> league;
+    private String name;
+
+    public LeagueTable(String name) {
+        this.name = name;
+        league = new ArrayList<>();
+    }
+
+
+    public void addTeam(T team) {
+        if (league.contains(team)) System.out.println(team.getName() + " is already added");
+        else league.add(team);
+    }
+
+    public void printOutInOrder() {
+        Collections.sort(league);
+        for (T t : league) {
+            System.out.println(t.getName() + " : " + t.ranking());
+        }
+    }
 }
--- src/com/alex/Main.java
+++ src/com/alex/Main.java
@@ -3,7 +3,42 @@ package com.alex;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        FootballPlayer fred = new FootballPlayer("Fred");
+        BaseballPlayer rob = new BaseballPlayer("Rob");
+        SoccerPlayer tom = new SoccerPlayer("Tom");
+
+        Team<SoccerPlayer> spartak = new Team<>("Spartak");
+        spartak.addPlayer(tom);
+        System.out.println(spartak.numPlayers());
+
+        Team<SoccerPlayer> loko = new Team<>("Loko");
+        Team<SoccerPlayer> fakel = new Team<>("Fakel");
+        Team<SoccerPlayer> krylya = new Team<>("Krylya");
+
+
+        loko.matchResult(fakel, 1,2);
+        spartak.matchResult(krylya, 4,3);
+        fakel.matchResult(fakel,1,0);
+        spartak.matchResult(loko,1,1);
+
+//        System.out.println("Rankings");
+//        System.out.println(spartak.getName() + ": " + spartak.ranking());
+//        System.out.println(loko.getName() + ": " + loko.ranking());
+//        System.out.println(fakel.getName() + ": " + fakel.ranking());
+//        System.out.println(krylya.getName() + ": " + krylya.ranking());
+//
+//        System.out.println(spartak.compareTo(loko));
+//        System.out.println(krylya.compareTo(fakel));
+
+        LeagueTable<Team<SoccerPlayer>> leagueTable = new LeagueTable("SoccerLeague");
+        leagueTable.addTeam(spartak);
+        leagueTable.addTeam(loko);
+        leagueTable.addTeam(fakel);
+        leagueTable.addTeam(krylya);
+
+//        leagueTable.addTeam(new Team<BaseballPlayer>("HPCh"));
+
+        leagueTable.printOutInOrder();
     }
 }
 // Create a generic class to implement a league table for a sport.
